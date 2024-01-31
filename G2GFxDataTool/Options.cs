7fd52476cb23fab0a6fc7e0ee583cb110a4a83c4
Notexe <josh@notex.app>
2024-01-31 16:46:59 +1000

    feat: initial barebones program

---
+++ G2GFxDataTool/Options.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace G2GFxDataTool
+{
+    public class Options
+    {
+        [Option('i', "input", SetName = "export", Required = false, HelpText = "Required. Path to the SWF file.")]
+        public string inputPath { get; set; }
+    }
+}
---
+++ G2GFxDataTool/Program.cs
@@ -0,0 +1,30 @@
+﻿using CommandLine;
+using SwfOp;
+
+namespace G2GFxDataTool
+{
+    internal class Program
+    {
+        public static List<string> errorLogs = new List<string>();
+
+        static void Main(string[] args)
+        {
+            var parser = Parser.Default.ParseArguments<Options>(args);
+
+            parser
+                .WithParsed(options =>
+                {
+                    if (options.inputPath != null)
+                    {
+                        string ext = Path.GetExtension(options.inputPath);
+                        if (ext == ".swf")
+                        {
+                            Console.WriteLine("SWF file found");
+                            ContentParser parser = new ContentParser(options.inputPath);
+                            parser.Run();
+                        }
+                    }
+                });
+        }
+    }
+}
