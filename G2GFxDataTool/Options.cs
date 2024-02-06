5a2179a5c02cff564e699b7411b2b75810fb291b
Notexe <josh@notex.app>
2024-02-06 14:16:50 +1000

    feat: update help text

--- G2GFxDataTool/Options.cs
+++ G2GFxDataTool/Options.cs
@@ -4,13 +4,13 @@ namespace G2GFxDataTool
 {
     public class Options
     {
-        [Option('i', "input", SetName = "export", Required = false, HelpText = "Required. Path to the SWF file.")]
+        [Option('i', "input", SetName = "export", Required = false, HelpText = "Required. Path to the SWF file or directory containing SWF files.")]
         public string inputPath { get; set; }
 
-        [Option('o', "output", SetName = "export", Required = false, HelpText = "Required. Path to output files.")]
-        public string outputPath { get; set; }
+        [Option('o', "output", SetName = "export", Required = false, HelpText = "Path to output the files (defaults to the current working directory).")]
+        public string outputPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "output");
 
-        [Option('s', "save-paths", SetName = "export", Required = false, HelpText = "Saves Event, Switch and SoundBank paths to events.txt, switches.txt and soundbanks.txt text files in the output path.")]
+        [Option('s', "save-paths", SetName = "export", Required = false, HelpText = "Saves Scaleform GFx and UIControl paths to scaleformgfx.txt and uicontrol.txt text files in the output directory.")]
         public bool savePaths { get; set; }
 
         [Option('l', "licenses", SetName = "licenses", Required = false, HelpText = "Prints license information for G2GFxDataTool and third party libraries that are used.")]
